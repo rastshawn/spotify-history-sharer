8a61ba298ede1876f77c3652146fd1024856d232
Shawn Rast <rastshawn@gmail.com>
2019-01-30 21:33:21 -0600

    task update

--- spotify-widget-server/index.js
+++ spotify-widget-server/index.js
@@ -20,25 +20,52 @@ process.argv.forEach((arg) => {
     } else if (arg.indexOf('testID') > -1){
         app.testID = arg.substr(7);
     }
+
+    // TODO rewrite this function
+    // to create one that does this that takes an array of 
+    // parameters to watch for
+
+    // and closes the app if any one is absent
 });
 
 if (!app.clientSecret || !app.clientID || !app.sessionSecret) {
     console.log("You must specify a clientID, clientSecret, and session secret.")
     console.log("nodejs index.js clientID=asfeijklsfe secret=sefjklfesjkl session=asdfjkles");
+    // TODO combine this function with the above function that parses launch arguments
+    // print the missing arg
     process.exit();
 }
 
+const mysql = require('mysql');
+const MySQLStore = require('express-mysql-session')(session);
+let mysqlOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'username',
+    password: 'password',
+    database: 'soundtrack-journal'    
+};
 
-app.users = [];
+const databaseConnection = mysql.createConnection(mysqlOptions);
+const sessionStore = new MySQLStore({
+        // options, read here:
+        // https://www.npmjs.com/package/express-mysql-session
 
+    },
+    databaseConnection);
 app.use(session({
     secret: app.sessionSecret,
     resave: false,
+    store: sessionStore,
     saveUninitialized: false,
     cookie: {
         //secure: true // uncomment when running on https
+        // TODO set secure to true
     }
 }));
+
+app.users = [];
+
 app.listen(port, () => {
     console.log(`server listening on ${port}`);
 });
@@ -76,6 +103,17 @@ app.get('/users/:userID/last50', (req, res) => {
             res.send(err);
         });
 });
+
+
+
+app.close = function(){
+    sessionStore.close();
+    // close db connections
+    // close all active user sessions with messages
+    // stop hosting
+    
+    process.exit();
+};
 
 test();
 function test() {
