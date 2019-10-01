6a7d2121a230c08036915073883efc13347191b8
Shawn Rast <rastshawn@gmail.com>
2019-09-30 23:24:21 -0500

    Added test Last50 page, first page of UI refactor.

---
+++ trackrecord-ui/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify';
+
+Vue.config.productionTip = false
+
+window.app = new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
