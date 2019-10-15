78c767469571af1483a4281ac5e65b97a1909d9d
Shawn Rast <rastshawn@gmail.com>
2019-10-14 22:47:51 -0500

    Adding basic routing

---
+++ trackrecord-ui/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <h1>HOME</h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  // data: () => {
+
+  // }
+  
+};
+</script>
--- trackrecord-ui/src/main.js
+++ trackrecord-ui/src/main.js
@@ -1,10 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+
+import Last50 from '@/components/Last50';
+import Home from '@/components/Home';
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/Last50',
+      name: 'Last50',
+      component: Last50 
+    }
+  ]
+});
+
+
+Vue.config.productionTip = false;
 
 window.app = new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
